14db48c21c620d54ff691bfb64813d77a0a37c25
nkoort <maks-g@i.ua>
2021-12-20 14:18:41 +0200

    master

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
 		$('.header__burger, .header__link').toggleClass('active');
 	});
 });
 
 // span-sotial
 
-$(document).ready(function() {
-	$('.span-sotial').click(function(event) {
+$(document).ready(function () {
+	$('.span-sotial').click(function (event) {
 		$('.span-sotial, .header__top-right-sotial, .header__top-right-sotials').toggleClass('active');
 	});
 });
 
 // span-menu
 
-$(document).ready(function() {
-	$('.span-menu').click(function(event) {
+$(document).ready(function () {
+	$('.span-menu').click(function (event) {
 		$('.span-menu, .header__menu-line,.header__menu-line-links, .header__menu-container').toggleClass('active');
 	});
 });
-$(document).ready(function() {
-	$('.span-menu').click(function(event) {
+$(document).ready(function () {
+	$('.span-menu').click(function (event) {
 		$('.header__menu-search').toggleClass('d-active');
 	});
 });
