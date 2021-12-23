766b459fef8b70ddd66f9717270ea09a797505bf
nkoort <maks-g@i.ua>
2021-12-23 15:09:40 +0200

    master

--- js/script.js
+++ js/script.js
@@ -25,9 +25,48 @@
 // 	});
 // });
 
+// .mobile
+// .ident
+// .interior
+// .ui
+
 $(document).ready(function () {
-	$(".link").click(function () {
-		$(".link, .block3__item").removeClass('active');
+	$(".all").click(function () {
+		$(".link").removeClass('active');
+		$(".item").removeClass('active');
 		$(this).toggleClass('active');
+		$('.item.all').toggleClass('active');
+	});
+});
+$(document).ready(function () {
+	$(".ident").click(function () {
+		$(".link").removeClass('active');
+		$(".item").removeClass('active');
+		$(this).toggleClass('active');
+		$('.item.ident').toggleClass('active');
+	});
+});
+$(document).ready(function () {
+	$(".mobile").click(function () {
+		$(".link").removeClass('active');
+		$(".item").removeClass('active');
+		$(this).toggleClass('active');
+		$('.item.mobile').toggleClass('active');
+	});
+});
+$(document).ready(function () {
+	$(".interior").click(function () {
+		$(".link").removeClass('active');
+		$(".item").removeClass('active');
+		$(this).toggleClass('active');
+		$('.item.interior').toggleClass('active');
+	});
+});
+$(document).ready(function () {
+	$(".ui").click(function () {
+		$(".link").removeClass('active');
+		$(".item").removeClass('active');
+		$(this).toggleClass('active');
+		$('.item.ui').toggleClass('active');
 	});
 });
